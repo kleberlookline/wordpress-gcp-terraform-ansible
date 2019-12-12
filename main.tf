1e15c76eaad544b10be97ff910adf5e5fe040a28
Kleber Oliveira <kleber.lookline@gmail.com>
2019-12-11 22:07:40 -0300

    Upload WordPress Terraform no GCP

--- main.tf
+++ main.tf
@@ -31,7 +31,3 @@ resource "google_sql_database_instance" "helloworld" {
   }
 }
 
-# Retorna o IP da VM criada
-output "IP_da_VM" {
-  value = "${google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip}"
-}
